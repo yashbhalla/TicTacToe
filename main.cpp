0ac165be4092032915b5f9b8585b442ec7bedf21
Mihika3601 <67787033+Mihika3601@users.noreply.github.com>
2021-10-01 11:06:40 +0530

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,42 @@
+#include "Classes.cpp"
+#include "Functions.cpp"
+
+int main(){
+	Board game;
+	string nameX, nameO; //player names
+	char winner = 'z'; //stores winner
+	int turns = 0; //counts number of turns
+	
+	cout<<"Welcome to tic-tac-toe!\n";
+	cout<<"\n---GAMEBOARD---\n";
+	printBlankBoard(); //prints board with numbered squares
+	
+	getUserNames(nameX, nameO);
+	while (turns <8) {
+		printBoard(game); // prints board
+		printUserPrompt(nameX, 'X'); //player X's turn
+		checkResponse(game, 'X'); //check if position is valid
+		winner = game.determineWinner(); //check for winner
+		
+		if (winner != 'z'){ //if player X has won, end the game
+			printBoard(game);
+			printWinner(game, nameX, nameO);
+			break;
+		}
+		
+		printBoard(game); //print board
+		printUserPrompt(nameO, 'O'); //player O's turn
+		checkResponse(game, 'O'); //check if position is valid
+		winner = game.determineWinner(); //check for winner
+		
+		if (winner != 'z'){ //if player O has won, end the game
+			printBoard(game);
+			printWinner(game, nameX, nameO);
+			break;
+		}
+		turns+=2;
+	}
+	if(turns >= 8)
+		cout<<"Game is tied!\n";
+	return 0;
+}
